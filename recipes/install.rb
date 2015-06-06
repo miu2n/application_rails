856872e517a46f0bcb19d01a54f1a31963e91ec2
Nikko Miu <miu2n@nikkomiu.com>
2015-06-06 10:14:12 -0400

    Updated install recipe

--- recipes/install.rb
+++ recipes/install.rb
@@ -5,13 +5,14 @@
 
 # Create shared capistrano folders
 node['application_rails']['capistrano_shared_folders'].each do |folder|
-  path = File.join(node['application_rails']['install_location'], 'shared', folder)
+  shared_path = File.join(node['application_rails']['install_location'], 'shared', folder)
+  app_path = File.join(node['application_rails']['install_location'], 'current', folder)
 
-  directory path do
+  directory shared_path do
     owner node['application_rails']['user']
     group node['application_rails']['group']
     mode 0755
-    not_if { File.exists?(path) }
+    not_if { File.exists?(shared_path) }
   end
 end if node['application_rails']['install_capistrano']
 
@@ -34,7 +35,7 @@ template File.join(config_path, 'database.yml') do
   source 'database.yml.erb'
   user node['application_rails']['user']
   group node['application_rails']['group']
-  mode 0700 # Set the secrets file to only be usable for the user
+  mode 0700 # Set the database file to only be usable for the user
 end
 
 # Create secrets config
@@ -48,12 +49,21 @@ end
 root_path = node['application_rails']['install_location']
 root_path = File.join(root_path, 'current') if node['application_rails']['install_capistrano']
 
+# Create directories for PID and SOCK files
+%w{'tmp/pids', 'tmp/sockets'}.each do |dir|
+  directory File.join(root_path, dir) do
+    owner node['application_rails']['user']
+    group node['application_rails']['group']
+    mode 0755
+    recursive true
+    not_if { File.exists?(path) }
+  end
+end
+
 # Create init
 execute 'foreman export' do
-  command 'foreman export'
+  command "foreman export upstart /etc/init -a #{node['application_rails']['app_name']} -u #{node['application_rails']['user']}"
   cwd root_path
-  user node['application_rails']['user']
-  group node['application_rails']['group']
 end
 
 # Load DB Schema
@@ -95,9 +105,7 @@ if node['application_rails']['rails_env'] == 'production'
 end
 
 # Start application
-execute 'foreman start' do
-  command 'foreman start'
+execute 'start app' do
+  command "start #{node['application_rails']['app_name']}"
   cwd root_path
-  user node['application_rails']['user']
-  group node['application_rails']['group']
 end
